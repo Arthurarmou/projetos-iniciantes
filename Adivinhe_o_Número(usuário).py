066af81480a631e4ed1da5ef49c85125cc7ff176
Arthurarmou <morettiarthur61@gmail.com>
2025-01-18 16:33:04 -0300

    Add files via upload

---
+++ Adivinhe_o_Número(usuário).py
@@ -0,0 +1,56 @@
+from random import randint
+def lernumero():
+    while True:
+        escolha = input("insira o número: ")
+        try:
+            numero = int(escolha)
+            return numero
+        except:
+            print("você não inseriu um número inteiro:")
+            continue
+numeroadv = randint(0,10)
+ultimonum = 0
+chances = 5
+tentativas = 0
+print("insira o número inteiro para adivinhação em baixo (número entre 0 e 10):")
+while True:
+    if chances == 0:
+        print("acabou o jogo suas chances, chegaram a 0")
+        continuar = input("você deseja continuar?. Se sim digite sim:").lower().strip()
+        if continuar == "sim" or continuar =="si" or continuar =="s":
+            chances += 5
+            tentativas = 0
+            continue
+        else:
+            print("fechando...")
+            break
+    escolha = lernumero()
+    if escolha > 10 or escolha < 0:
+        print(f"está incorreto insira um número entre 0 e 10 e não: {escolha}")
+        continue
+    if escolha == ultimonum: #para verificar se o último número é igual 
+        print("o número inserido está incorreto tem que ser diferente!")
+        continue
+
+    ultimonum = escolha
+
+    if escolha > numeroadv:
+        print(f"o número é menor que {escolha}")
+        chances -= 1
+        tentativas += 1
+        print(f"você ainda tem {chances} chances")
+        continue
+    elif escolha < numeroadv:
+        print(f"o número é maior que {escolha}")
+        chances -= 1
+        tentativas +=1
+        print(f"você ainda tem {chances} chances")
+        continue
+    else:
+        print(f"o número está correto!: {escolha}, você acertou em {tentativas} tentativas")
+        chances = 5
+        tentativas = 0
+    continuar = input("insira se deseja continuar:").lower().strip()
+    if not continuar == "sim" or continuar =="si" or continuar =="s":
+        print("fechando o programa...")
+        break
